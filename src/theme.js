37875d95e2b22e865d68a1b13504f6ea2b6d9314
Magdalena Zulewska <madziapernach@gmail.com>
2022-11-30 12:37:12 +0100

    features/ MovieTile

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+export const theme = {
+  colors: {
+    black: "#18181B",
+    darkGrey: "#7E839A",
+    grey: "#E4E6F0",
+    lightGrey: "#F5F5FA",
+    white: "#FFFFFF",
+    blue: "#0044CC",
+    lightBlue: "#D6E4FF",
+    yellow: "#FCD420",
+  },
+  breakpoints: {
+    mobileMin: 320,
+    mobileMax: 767,
+  },
+  radius: {
+    small: "5px",
+    navigationInput: "33px",
+  },
+  shadow: {
+    mainShadow: "0px 4px 12px rgba(186, 199, 213, 0.5);",
+  },
+};
